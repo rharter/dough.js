930de7398ccb773bdd6f79bd953fee32ecdcac3c
Ryan Harter <ryanjharter@gmail.com>
2012-07-31 19:47:45 -0500

    Added authenticated routing.

--- src/main/resources/javascript/dough-router.js
+++ src/main/resources/javascript/dough-router.js
@@ -2,6 +2,56 @@
 
 namespace('dough.router');
 
+/**
+ * Represents a route to handle web requests that requires authentication.
+ *
+ * @param {string} pattern       A regex pattern to identify the URL patterns that
+ *                               should be handled by handler.
+ * @param {function} handler     A JavaScript function to handle matched routes.
+ * @param {[string]} methods     An array of method strings to matches, or 
+ *                               null to match all methods.
+ * @param {string} auth_redirect The url to redirect unathenticated requests to if
+ *                               this is a protected route, otherwise null.
+ * @constructor
+ */
+dough.router.Route = function (pattern, handler, methods, auth_redirect) {
+	this.pattern = pattern;
+	this.handler = handler;
+	this.methods = methods || ['GET', 'POST', 'PUT', 'HEAD', 
+	                           'OPTIONS', 'DELETE', 'TRACE', 
+	                           'CONNECT'];
+	this.auth_redirect = auth_redirect;
+}
+
+dough.router.Route.prototype = {
+	/**
+	 * Extracts the params from the request url and adds
+	 * them to the request parameters.
+	 */
+	evaluateParams: function (request) {
+    	var path_tokens = dough.router.get_tokens(request.target);
+		var route_tokens = dough.router.get_tokens(this.pattern);
+
+        var params = {};
+        for (var j = path_mark = 0; j <= route_tokens.length; j++, path_mark++) {
+            var item = route_tokens[j];
+
+            // Got a variable in the route
+            if (item === ':' && path_tokens[j] !== undefined) {
+                var name = route_tokens[++j];
+                params[name] = path_tokens[path_mark];
+                continue;
+            }
+        }
+
+        for (var p in params) {
+			request.params[p] = params[p];
+		}
+		
+		return request;
+	}
+}
+
 /** 
  * Hash containing the registered routes
  */
@@ -17,11 +67,24 @@ dough.router.routes = dough.router.routes || [];
  *     null to match all methods.
  */
 dough.router.register = function (pattern, handler, methods) {
-    var route = {};
-    route.pattern = pattern;
-    route.methods = methods;
-    route.handler = handler;
-    dough.router.routes.push(route);
+    var route = new dough.router.Route(pattern, handler, methods);
+	dough.router.routes.push(route);
+}
+
+/**
+ * Registers a route to handle web requests that requires authentication.
+ *
+ * @param {string} pattern   A regex pattern to identify the URL patterns that
+ *                           should be handled by handler.
+ * @param {function} handler A JavaScript function to handle matched routes.
+ * @param {[string]} methods An array of method strings to matches, or 
+ *                           null to match all methods.
+ * @param {string} redirect  The url to redirect the user to if they aren't 
+ *                           authenticated.
+ */
+dough.router.registerAuthenticatedRoute = function (pattern, handler, methods, redirect) {
+	var route = new dough.router.Route(pattern, handler, methods, redirect);
+	dough.router.routes.push(route);
 }
 
 /** 
@@ -30,15 +93,14 @@ dough.router.register = function (pattern, handler, methods) {
  * If route.methods is null, then any matching path will be returned.
  */
 dough.router.resolve = function (path, method) {
-    var path_tokens = get_tokens(path);
-
+    var path_tokens = dough.router.get_tokens(path);
     var routes = dough.router.routes;
     for (var i = 0; i < routes.length; i++) {
         var route = routes[i];
 
         // ensure the route handles this method
         if (route.methods === undefined || route.methods.indexOf(method) > -1) {
-            var route_tokens = get_tokens(route.pattern);
+            var route_tokens = dough.router.get_tokens(route.pattern);
 
             var match = true;
             var params = {};
@@ -68,13 +130,13 @@ dough.router.resolve = function (path, method) {
             }
 
             if (match) {
-				return {handler: route.handler, params: params};
+				return route;
             }
         }
     }
 };
 
-function get_tokens(str) {
+dough.router.get_tokens = function (str) {
     var tokens = [];
     var delims = ['/',':'];
     
--- src/main/resources/javascript/dough-server.js
+++ src/main/resources/javascript/dough-server.js
@@ -11,6 +11,7 @@ dough.server.start = function() {
                              Packages.org.eclipse.jetty.server,
                              Packages.org.eclipse.jetty.server.handler,
                              Packages.org.eclipse.jetty.server.nio,
+							 Packages.org.eclipse.jetty.server.session,
                              Packages.org.eclipse.jetty.util.thread);
 
     with (jetty) {
@@ -31,6 +32,8 @@ dough.server.start = function() {
 		// Add the handlers
 		var handlers = new HandlerList();
 		
+		handlers.addHandler(new SessionHandler());
+		
 		var publicHandler = new ResourceHandler();
 		publicHandler.setResourceBase(dough.global.root + '/public/');
 		handlers.addHandler(publicHandler);
@@ -46,13 +49,14 @@ dough.server.start = function() {
 
 dough.server.handle = function(java_target, java_base_request, java_request, java_response) {
     var request = new dough.server._Request(java_request);
+	request.target = java_target + '';
 
 	if (dough.server.sessionHandler) {
 		request.session = dough.server.sessionHandler
-				.getSession(java_request.getSession(true).getId());
+				.getSession(request._request.getSession(true).getId());
 	}
 
-    out.println("Started " + request.method + " \"" + request.target + "\" for " + request.client_address.address + " at " + new Date());
+    out.println("Started " + request.method + " \"" + request.target + "\" for " + request.client_address.address + " at " + new Date() + "\n");
 
     var route = dough.router.resolve(request.target, request.method);;
     
@@ -60,12 +64,15 @@ dough.server.handle = function(java_target, java_base_request, java_request, jav
         out.println("\nNo route matches [" + request.method + "] \"" + request.target + "\"\n");
         return;
     }
-
-    out.println("Processing with " + JSON.stringify(route));
-
-	for (var p in route.params) {
-		request.params[p] = route.params[p];
+	
+	// If this is a protected route and there isn't a user, redirect
+	if (route.auth_redirect && request.session && !request.session.val('user_id')) {
+		java_response.sendRedirect(route.auth_redirect + '?forward_url=' + request.target);
 	}
+
+    out.println("Processing with " + JSON.stringify(route.pattern));
+
+	request.params = route.evaluateParams(request);
 	var ret = route.handler(request, java_response);
 
     if (ret !== undefined) {
@@ -86,14 +93,13 @@ dough.server.handle = function(java_target, java_base_request, java_request, jav
 dough.server._Request = function (request) {
 	this._request = request;
 
-	this.method = java_request.getMethod() + "";
-	this.target = java_target + "";
-	this.query = java_request.getQueryString() + "";
-	this.path = java_request.getPathInfo() + "";
+	this.method = request.getMethod() + "";
+	this.query = request.getQueryString() + "";
+	this.path = request.getPathInfo() + "";
 	this.client_address = {
-	    port: java_request.getRemotePort(),
-	    host: java_request.getRemoteHost(),
-	    address: java_request.getRemoteAddr()
+	    port: request.getRemotePort(),
+	    host: request.getRemoteHost(),
+	    address: request.getRemoteAddr()
 	};
 	
 	this.params = {};
@@ -101,20 +107,9 @@ dough.server._Request = function (request) {
 	for each(var n in names) {
 		var v = request.getParameterValues(n);
 		if (v.length === 1) {
-			this.request.params[n] = v[0] + "";
+			this.params[n] = v[0] + "";
 		} else {
-			this.request.params[n] = v.slice().map(function (el) { return el + '' });
+			this.params[n] = v.slice().map(function (el) { return el + '' });
 		}
 	}
-}
-
-dough.server._Request.prototype = {
-	this._request: null,
-	this.method: null,
-	this.target: null,
-	this.query: null,
-	this.path: null,
-	this.client_address: null,
-	this.params: null,
-	this.session: null
 }
